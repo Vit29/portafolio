449048fdb8baf99444d61885bbb9e3eee0ac53a7
Vitaly <vitaly.varela@hotmail.com>
2024-04-23 21:27:03 -0600

    repartiendo funcion showcard en mas funciones

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ nav.addEventListener('click', () => {
     nav.classList.remove('active')
 })
 
-const gameOver = 12;
+const GAMEOVER = 12;
 const MOBILE_SCREEN_WIDTH = 758;
 const ANIMATION_DURATION = 12000;
 
@@ -24,98 +24,19 @@ let cardOne = null
 let cardTwo = null
 let resultOne = null;
 let resultTwo = null;
-let successes = 0;
+let SUCCESSES = 0;
 let numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12];
 
 numbers = numbers.sort(()=> {
     return Math.random() - 0.5
 })
 
-cards.forEach(element => {
-    element.addEventListener('click',()=> {
-        const id = element.id
+cards.forEach( card => {
+     card.addEventListener('click', () => {
+        const id =  card.id
         showCard(id);
     })
 });
-
-function winner () {
-    winnerSound.play();
-    cards.forEach((card) => {
-        card.classList.add('winAnimation');
-        setTimeout(() => {
-            card.classList.remove('winAnimation')
-        },ANIMATION_DURATION)
-    })
-    const end = Date.now() + ANIMATION_DURATION
-    document.body.clientWidth < MOBILE_SCREEN_WIDTH
-    ? animationWinnerMovil(end)
-    : animationWinner(end)
-}
-
-function showCard (id) {
-    count++;
-    if ( count == 1) {
-        cardOne = document.getElementById(id);
-        resultOne = numbers[id];
-        cardOne.innerHTML = `<img src="skills/img/${resultOne}.png">`;
-        cardOne.disabled = true;
-        //color border 
-        cardOne.style.border = 'solid 1px yellow';
-        // color shadow 
-        cardOne.style.boxShadow = '0 0 10px 1px yellow';
-        selected.play()
-        scaleAnimation(cardOne)
-    } else if (count == 2) {
-        cardTwo = document.getElementById(id);
-        resultTwo = numbers[id];
-        cardTwo.innerHTML = `<img src="skills/img/${resultTwo}.png">`;
-        cardTwo.disabled = true;
-        //color border
-        cardTwo.style.border = 'border 1px yellow';
-        // color shadow
-        cardTwo.style.boxShadow = '0 0 10px 1px yellow';
-        
-        if (resultOne == resultTwo) {
-            count = 0;
-            // color borde
-            cardOne.style.border = 'solid 1px green';
-            cardTwo.style.border = 'solid 1px green';
-            // color sombra
-            cardOne.style.boxShadow = '0 0 10px 1px green';
-            cardTwo.style.boxShadow = '0 0 10px 1px green';
-            correct.play();
-            correctAnswerAnimate(cardOne,cardTwo);
-            successes++;
-            if (successes == gameOver) {
-                winner()
-            }
-        } else {
-            //color border
-            cardOne.style.border = 'solid 1px red';
-            cardTwo.style.border = 'solid 1px red';
-            //color shadow
-            cardOne.style.boxShadow = '0 0 10px 1px red';
-            cardTwo.style.boxShadow = '0 0 10px 1px red';
-            //audio
-            fail.play()
-            failSoundAnimate(cardOne,cardTwo)
-            setTimeout(()=> {
-                cardOne.innerHTML = '';
-                cardTwo.innerHTML = '';
-                cardOne.disabled = false;
-                cardTwo.disabled = false;
-                count = 0;
-                // colocar cielueta 
-                cardOne.style.border = 'dashed 3px white';
-                cardTwo.style.border = 'dashed 3px white';
-                // remover sombra
-                cardOne.style.boxShadow = 'none';
-                cardTwo.style.boxShadow = 'none';
-                // element.classList.remove('animate');
-            },1000)
-        }
-    }
-}
 
 function scaleAnimation (cardOne) {
     cardOne.classList.add('scaleAnimate');
@@ -137,6 +58,88 @@ function correctAnswerAnimate (cardOne, cardTwo) {
     cardOne.classList.add('correctAnimation');
     cardTwo.classList.add('correctAnimation');
 }
+
+function winner () {
+    winnerSound.play();
+    cards.forEach((card) => {
+        card.classList.add('winAnimation');
+        setTimeout(() => {
+            card.classList.remove('winAnimation')
+        },ANIMATION_DURATION)
+    })
+    const end = Date.now() + ANIMATION_DURATION
+    document.body.clientWidth < MOBILE_SCREEN_WIDTH
+    ? animationWinnerMovil(end)
+    : animationWinner(end)
+}
+
+function successes () {
+    count = 0;
+    SUCCESSES++;
+    correct.play();
+    correctAnswerAnimate(cardOne,cardTwo);
+    cardOne.style.border = 'solid 1px green';
+    cardOne.style.boxShadow = '0 0 10px 1px green';
+    cardTwo.style.border = 'solid 1px green';
+    cardTwo.style.boxShadow = '0 0 10px 1px green';
+    if (SUCCESSES == GAMEOVER) {
+        winner()
+    }
+}
+
+function showCardOne (id) {
+    cardOne = document.getElementById(id);
+    resultOne = numbers[id];
+    cardOne.innerHTML = `<img src="skills/img/${resultOne}.png">`;
+    cardOne.disabled = true;
+    cardOne.style.border = 'solid 1px yellow';
+    cardOne.style.boxShadow = '0 0 10px 1px yellow';
+    selected.play()
+    scaleAnimation(cardOne)
+}
+
+function showCardTwo (id) {
+    cardTwo = document.getElementById(id);
+    resultTwo = numbers[id];
+    cardTwo.innerHTML = `<img src="skills/img/${resultTwo}.png">`;
+    cardTwo.disabled = true;
+    cardTwo.style.border = 'border 1px yellow';
+    cardTwo.style.boxShadow = '0 0 10px 1px yellow';
+}
+
+function cleanCards () {
+    cardOne.style.border = 'solid 1px red';
+    cardTwo.style.border = 'solid 1px red';
+    cardOne.style.boxShadow = '0 0 10px 1px red';
+    cardTwo.style.boxShadow = '0 0 10px 1px red';
+    fail.play()
+    failSoundAnimate(cardOne,cardTwo)
+    setTimeout(()=> {
+        cardOne.innerHTML = '';
+        cardTwo.innerHTML = '';
+        cardOne.disabled = false;
+        cardTwo.disabled = false;
+        count = 0;
+        cardOne.style.border = 'dashed 3px white';
+        cardTwo.style.border = 'dashed 3px white';
+        cardOne.style.boxShadow = 'none';
+        cardTwo.style.boxShadow = 'none';
+    },1000)
+}
+
+function showCard (id) {
+    count++;
+    if ( count == 1) {
+        showCardOne(id)
+    } 
+    if (count == 2) {
+        showCardTwo(id)
+        resultOne == resultTwo
+        ? successes()
+        : cleanCards()
+    }
+}
+
 
 function animationWinner(end) {
     (function frame() {
